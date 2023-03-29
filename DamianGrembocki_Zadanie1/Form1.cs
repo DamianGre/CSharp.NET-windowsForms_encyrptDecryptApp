80b35a3957b62254f555a7e78524f24f4aebeda9
Damian Grembocki <damian.grembocki@o2.pl>
2023-03-29 11:10:58 +0200

    commit

--- DamianGrembocki_Zadanie1/Form1.cs
+++ DamianGrembocki_Zadanie1/Form1.cs
@@ -59,52 +59,52 @@ namespace DamianGrembocki_Zadanie1
                 label4.Text = saveFileDialog1.FileName;
             }
         }
-        
-         private void button2_Click(object sender, EventArgs e)
-         {
-             textBox2.Text = EncryptString(textBox1.Text);
-         }
-         private void button4_Click(object sender, EventArgs e)
-         {
-             textBox2.Text = DecryptString(textBox1.Text);
-         }            
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = EncryptString(textBox1.Text);
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = DecryptString(textBox1.Text);
+        }
 
         public string EncryptString(string inputString)
         {
             MemoryStream memStream = null;
-            
-                byte[] key = { };
-                byte[] IV = { 12, 21, 43, 17, 57, 35, 67, 27 };
-                string encryptKey = "aXb2uy4z"; // MUST be 8 characters
-                key = Encoding.UTF8.GetBytes(encryptKey);
-                byte[] byteInput = Encoding.UTF8.GetBytes(inputString);
-                DESCryptoServiceProvider provider = new DESCryptoServiceProvider();
-                memStream = new MemoryStream();
-                ICryptoTransform transform = provider.CreateEncryptor(key, IV);
-                CryptoStream cryptoStream = new CryptoStream(memStream, transform, CryptoStreamMode.Write);
-                cryptoStream.Write(byteInput, 0, byteInput.Length);
-                cryptoStream.FlushFinalBlock();
-           
+
+            byte[] key = { };
+            byte[] IV = { 12, 21, 43, 17, 57, 35, 67, 27 };
+            string encryptKey = "aXb2uy4z"; // MUST be 8 characters
+            key = Encoding.UTF8.GetBytes(encryptKey);
+            byte[] byteInput = Encoding.UTF8.GetBytes(inputString);
+            DESCryptoServiceProvider provider = new DESCryptoServiceProvider();
+            memStream = new MemoryStream();
+            ICryptoTransform transform = provider.CreateEncryptor(key, IV);
+            CryptoStream cryptoStream = new CryptoStream(memStream, transform, CryptoStreamMode.Write);
+            cryptoStream.Write(byteInput, 0, byteInput.Length);
+            cryptoStream.FlushFinalBlock();
+
             return Convert.ToBase64String(memStream.ToArray());
         }
 
         public string DecryptString(string inputString)
         {
             MemoryStream memStream = null;
-            
-                byte[] key = { };
-                byte[] IV = { 12, 21, 43, 17, 57, 35, 67, 27 };
-                string encryptKey = "aXb2uy4z"; // MUST be 8 characters
-                key = Encoding.UTF8.GetBytes(encryptKey);
-                byte[] byteInput = new byte[inputString.Length];
-                byteInput = Convert.FromBase64String(inputString);
-                DESCryptoServiceProvider provider = new DESCryptoServiceProvider();
-                memStream = new MemoryStream();
-                ICryptoTransform transform = provider.CreateDecryptor(key, IV);
-                CryptoStream cryptoStream = new CryptoStream(memStream, transform, CryptoStreamMode.Write);
-                cryptoStream.Write(byteInput, 0, byteInput.Length);
-                cryptoStream.FlushFinalBlock();
-           
+
+            byte[] key = { };
+            byte[] IV = { 12, 21, 43, 17, 57, 35, 67, 27 };
+            string encryptKey = "aXb2uy4z"; // MUST be 8 characters
+            key = Encoding.UTF8.GetBytes(encryptKey);
+            byte[] byteInput = new byte[inputString.Length];
+            byteInput = Convert.FromBase64String(inputString);
+            DESCryptoServiceProvider provider = new DESCryptoServiceProvider();
+            memStream = new MemoryStream();
+            ICryptoTransform transform = provider.CreateDecryptor(key, IV);
+            CryptoStream cryptoStream = new CryptoStream(memStream, transform, CryptoStreamMode.Write);
+            cryptoStream.Write(byteInput, 0, byteInput.Length);
+            cryptoStream.FlushFinalBlock();
+
 
             Encoding encoding1 = Encoding.UTF8;
             return encoding1.GetString(memStream.ToArray());
@@ -163,5 +163,10 @@ namespace DamianGrembocki_Zadanie1
             cryptoStream.Close();
             return Encoding.UTF8.GetString(plainTextBytes, 0, decryptedByteCount).TrimEnd("\0".ToCharArray());
         }
-    }        
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
 }
